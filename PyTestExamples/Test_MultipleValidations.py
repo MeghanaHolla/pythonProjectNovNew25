94ac4cac3ef7d469030a77520686bf232ec2d614
Meghana <meghanaholla8526@gmail.com>
2020-11-25 13:01:56 +0530

    Check

--- PyTestExamples/Test_MultipleValidations.py
+++ PyTestExamples/Test_MultipleValidations.py
@@ -13,7 +13,7 @@ def init_driver():
     yield
     driver.close()
 
-#New Code insert to Test GIT Hub
+#New Code insert to Test PULL Request
 def test_verify_page_title(init_driver):
     actual_title = driver.title
     assert actual_title == "Job Vacancy - Latest Job Openings - Job Search Online - Monster India"
